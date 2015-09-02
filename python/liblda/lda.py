32b4c6445a231ef35378133561841bb97a0d7958
hedi7 <Heini@sjtu.edu.cn>
2015-09-02 15:53:46 +0800

    Added new lda class, modified some other aspects

---
+++ python/liblda/lda.py
@@ -0,0 +1,30 @@
+from liblda import fitlda, predictldafromutterance, predictldafromarray
+# the LDA class can be used by simply initzialize it and use the fit and
+# predict parameters
+
+
+class LDA():
+
+    def fit(self, flist, dim):
+        '''
+        Estimates an LDA matrix from the given flist and transforms that matrix to dimension dim
+        flist is a dict where the keys are the given speakers ( how many classes there are  ) and
+        the values correspond to the HTK type features as paths
+        '''
+        self._ldamat = fitlda(flist, dim)
+
+    def predict(self, featurefile):
+        '''
+        Predicts for the given file given as f
+        '''
+        if self._ldamat is None:
+            raise ValueError(
+                "THe method .fit needs to be called before predict!")
+        return predictldafromutterance(featurefile, self._ldamat)
+
+    def predictmat(self, featuremat):
+
+        if self._ldamat is None:
+            raise ValueError(
+                "THe method .fit needs to be called before predict!")
+        return predictldafromarray(featuremat, self._ldamat)
