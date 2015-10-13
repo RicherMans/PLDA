8c2c4b0038c3d3b4cc08c6b5a91b7c1115c19884
hedi7 <Heini@sjtu.edu.cn>
2015-10-13 18:28:36 +0800

    Made the test clearer

--- tests/pldatest.py
+++ tests/pldatest.py
@@ -9,17 +9,26 @@ class PLDATest(unittest.TestCase):
         self.labels = np.array([i%10 for i in range(self.data.shape[0])])
     def test_fittransform(self):
         self.assertIsNone(self.pldamodule.fit(self.data,self.labels))
-        testdata = np.random.rand(100,10)
-        testlabels = np.array([i%10 for i in range(testdata.shape[0])])
-        transformed = self.pldamodule.transform(testdata,testlabels)
+        enroledata = np.random.rand(100,10)
+        enrolelabels = np.array([i%10 for i in range(enroledata.shape[0])])
+        transformed = self.pldamodule.transform(enroledata,enrolelabels)
+	testdata = np.random.rand(100,10)
+	testlabels=np.arange(100)
+
+	transformedtest = self.pldamodule.transform(testdata,testlabels)
+
+        self.assertEqual(len(transformedtest.keys()),100)
         self.assertEqual(len(transformed.keys()),10)
 
         bkgdata = np.random.rand(5,10)
         self.pldamodule.norm(bkgdata,transformed)
         # random input
-        score = self.pldamodule.score(transformed.keys()[0],transformed.values()[0],transformed.values()[2])
-        # Should be around this range
-        self.assertTrue( -100 <= score <= 100)
+        for model,modelvec in transformed.iteritems():
+            for testuttname,testvec in transformedtest.iteritems():
+                score=self.pldamodule.score(model,modelvec,testvec)
+                # Should be around this range
+                self.assertTrue( -100 <= score <= 100)
+
 
 
 if __name__ == '__main__':
